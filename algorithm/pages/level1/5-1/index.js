bb042891eea777854b1f6fd01d9b968056d7ace1
Quokka <sm.kim@last30min.com>
2022-05-23 10:09:13 +0900

    최대공약수와 최소공배수 풀이 수정

--- algorithm/pages/level1/5-1/index.js
+++ algorithm/pages/level1/5-1/index.js
@@ -7,44 +7,30 @@ export default function Algorithm() {
   let nMeasure = [];
   let mMeasure = [];
 
+  //n의 약수를 구한다
   for (let i = 0; i <= n; i++) {
     if (n % i === 0) {
       nMeasure.push(i);
     }
   }
 
+  //m의 약수를 구한다
   for (let i = 0; i <= m; i++) {
     if (m % i === 0) {
       mMeasure.push(i);
     }
   }
 
-  const aaa = [
-    nMeasure.filter((data) => mMeasure.includes(data)).reduce((a, b) => a * b),
-  ];
+  // n과 m의 약수중 공통된 부분만 추출한다.
+  const commonMeasure = nMeasure.filter((data) => mMeasure.includes(data));
 
-  const bbb = [(n * m) / aaa];
+  //공약수 중에 최대값을 추출한다.
+  const maxMeasure = Math.max.apply(null, commonMeasure);
 
-  const answer = aaa.concat(bbb);
-  console.log(answer);
+  //최소 공배수 공식 => 두 수를 곱한다음 최대공약수로 나누면 최소공배수가 됨.
+  const minMultiple = [(n * m) / maxMeasure];
 
-  // //최소공배수
-  // let nMultiple = [];
-  // let mMultiple = [];
-  //
-  // let 최소공배수 = "";
-  //
-  // for (let i = 0; i < 10000000000; i++) {
-  //   const aaa = nMultiple.filter((data) => mMultiple.includes(data));
-  //   if (aaa.length === 1) {
-  //     최소공배수 = aaa;
-  //     break;
-  //   }
-  //   nMultiple.push(n + n * i);
-  //   mMultiple.push(m + m * i);
-  // }
-  //
-  // const answer = 최대공약수.concat(최소공배수);
-  //
-  // console.log(answer);
+  const answer = [maxMeasure].concat(minMultiple);
+
+  return answer;
 }
