dd7699a81eb513bceca8114d83c2a8211e9d63fe
Quokka <sm.kim@last30min.com>
2022-06-22 19:33:50 +0900

    문자열 내 마음대로 정렬하기 풀이

--- algorithm/pages/level1/9-2/index.js
+++ algorithm/pages/level1/9-2/index.js
@@ -1,3 +1,8 @@
 export default function Algorithm() {
   //문지열 내 마음대로 정렬하기
+
+  const strings = ["sun", "bed", "car"];
+  const n = 1;
+
+  console.log(strings.sort().sort((a, b) => a.charCodeAt(n) - b.charCodeAt(n)));
 }
